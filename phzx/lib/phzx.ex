2c615ed3294ee22a65908e91caaed79a469f77bf
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-10-11 23:34:04 +0800

    create new project phzx

---
+++ phzx/lib/phzx.ex
@@ -0,0 +1,18 @@
+defmodule Phzx do
+  @moduledoc """
+  Documentation for Phzx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Phzx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ phzx/test/phzx_test.exs
@@ -0,0 +1,8 @@
+defmodule PhzxTest do
+  use ExUnit.Case
+  doctest Phzx
+
+  test "greets the world" do
+    assert Phzx.hello() == :world
+  end
+end
