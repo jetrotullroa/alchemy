0e8101b210a19e266955dc29fa5568621420ccbf
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-10-14 16:34:39 +0800

    setup calcs module

---
+++ phzx/lib/phzx/calcs.ex
@@ -0,0 +1,15 @@
+defmodule Calcs do
+
+  def square_root(val) do
+    :math.sqrt(val)
+  end
+
+  def squared(val) do
+    val * val
+  end
+
+  def cubed(val) do
+    val * val * val
+  end
+
+end
--- phzx/lib/phzx/rocketry.ex
+++ phzx/lib/phzx/rocketry.ex
@@ -1,28 +1,32 @@
 defmodule Phxz.Rocketry do
 
+  import Converter
+  import Calcs
+
+  @moon %{mass: 7.35e22, radius: 1.738e6}
+  @mars %{mass: 6.39e23, radius: 3.4e6}
+  @earth %{mass: 5.972e24, radius: 6.371e6}
+  @newton_constant 6.67e-11
+
   def escape_velocity(planet) when is_atom(planet) do
     case planet do
-      :earth -> %{mass: 5.972e24, radius: 6.371e6}
-      :mars -> %{mass: 6.39e23, radius: 3.4e6}
-      :moon -> %{mass: 7.35e22, radius: 1.738e6}
+      :earth -> @earth
+      :mars -> @mars
+      :moon -> @moon
     end |> escape_velocity
-  end
-
-  def escape_velocity(:mars) do
-
   end
 
   def escape_velocity(planet) do is_map(planet)
     planet
     |> calculate_escape
-    |> Converter.convert_to_km
-    |> Converter.rounded_to_the_nearest_tenth
+    |> convert_to_km
+    |> rounded_to_the_nearest_tenth
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    newton_constant = 6.67e-11
-    2 * newton_constant * mass / radius
-    |> :math.sqrt
+
+    2 * @newton_constant * mass / radius
+    |> square_root
   end
 
 end
