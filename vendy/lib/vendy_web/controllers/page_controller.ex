b253312e9dd4ef56131f0b02d05228e8b0d33cc8
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-10-23 21:48:53 +0800

    consume data via controller and pass it to view. make test pass

--- vendy/lib/vendy_web/controllers/page_controller.ex
+++ vendy/lib/vendy_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule VendyWeb.PageController do
   use VendyWeb, :controller
+  alias Vendy.Catalog
 
   def index(conn, _params) do
-    render conn, "index.html"
+    seasonal_products = Catalog.list_seasonal_products
+    render conn, "index.html", seasonal_products: seasonal_products
   end
 end
