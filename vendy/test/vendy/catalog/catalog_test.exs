223302e32af061f14ac3416fd2b281f176b467af
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-10-23 21:47:13 +0800

    create test that will show only seasanl products in homepage

---
+++ vendy/test/vendy/catalog/catalog_test.exs
@@ -0,0 +1,19 @@
+defmodule Vendy.CatalogTest do
+  use ExUnit.Case
+  alias Vendy.Catalog
+  alias Vendy.Catalog.Product
+
+  test "list_products/0 returns all products" do
+    [p1 = %Product{}, p2 = %Product{}] = Catalog.list_products
+    assert p1.name === "Tomato"
+    assert p1.price === "23"
+    assert p2.name === "Apple"
+    assert p2.price === "9"
+  end
+
+  test "list_seasonal_products/0 return only seasonal products" do
+    [product = %Product{}] = Catalog.list_seasonal_products
+    refute product.name === "Tomato"
+  end
+
+end
--- vendy/test/vendy_web/acceptance/homepage_test.exs
+++ vendy/test/vendy_web/acceptance/homepage_test.exs
@@ -13,7 +13,7 @@ defmodule VendyWeb.Acceptance do
     product_name = find_within_element(product, :css, ".product-name") |> visible_text
     product_price = find_within_element(product, :css, ".product-price") |> visible_text
     assert product_name === "Apple"
-    assert product_price === "23"
+    assert product_price === "9"
     refute product_name === "Tomato"
   end
 end
