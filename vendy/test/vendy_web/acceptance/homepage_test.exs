c867c9efd6369bbcf82bec19650b8789fd8c65e7
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-10-23 19:06:12 +0800

    add test that show product name and price then make it pass

--- vendy/test/vendy_web/acceptance/homepage_test.exs
+++ vendy/test/vendy_web/acceptance/homepage_test.exs
@@ -6,7 +6,14 @@ defmodule VendyWeb.Acceptance do
 
   test "presence of featured products" do
     navigate_to("/")
-    assert page_source =~ "Seasonal Products"
-  end
+    page_title = find_element(:css, ".page-title") |> visible_text
+    assert page_title === "Seasonal Products"
 
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text
+    assert product_name === "Apple"
+    assert product_price === "23"
+    refute product_name === "Tomato"
+  end
 end
