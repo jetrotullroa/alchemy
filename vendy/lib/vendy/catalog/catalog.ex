eaf799f4406a47e68621e9ede121d453282569fc
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-10-25 15:00:12 +0800

    create function to fetch products based on category

--- vendy/lib/vendy/catalog/catalog.ex
+++ vendy/lib/vendy/catalog/catalog.ex
@@ -1,14 +1,20 @@
 defmodule Vendy.Catalog do
   alias Vendy.Catalog.Product
+
   def list_products do
     [
-      %Product{name: "Tomato", price: "23", is_seasonal: false},
-      %Product{name: "Apple", price: "9", is_seasonal: true}
+      %Product{name: "Tomato", price: "23", is_seasonal: false, category: "vegetables"},
+      %Product{name: "Apple", price: "9", is_seasonal: true, category: "fruits"}
     ]
   end
 
   def list_seasonal_products do
     list_products
     |> Enum.filter( fn(product) -> product.is_seasonal === true end )
+  end
+
+  def get_category_product(category) do
+    list_products
+    |> Enum.filter( fn(product) -> product.category === category end)
   end
 end
--- vendy/lib/vendy/catalog/product.ex
+++ vendy/lib/vendy/catalog/product.ex
@@ -1,3 +1,3 @@
 defmodule Vendy.Catalog.Product do
-  defstruct [:name, :price, :is_seasonal]
+  defstruct [:name, :price, :is_seasonal, :category]
 end
