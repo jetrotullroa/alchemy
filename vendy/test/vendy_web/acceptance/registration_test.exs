05db1bb50dc3216148163461988be2c9ce7b92c1
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-11-04 18:28:42 +0800

    create acceptance test for the registration form

--- vendy/test/vendy_web/acceptance/registration_test.exs
+++ vendy/test/vendy_web/acceptance/registration_test.exs
@@ -1,15 +1,17 @@
 defmodule VendyWeb.Acceptance.RegistrationTest do
   use Vendy.DataCase
   use Hound.Helpers
+  alias Vendy.CRM
 
   hound_session()
 
   test "register an account with valid data" do
     navigate_to("/register")
-    form = find_element(:id, "form-registration")
+    form = find_element(:id, "registration-form")
     find_within_element(form, :name, "registration[name]") |> fill_field("John")
     find_within_element(form, :name, "registration[email]") |> fill_field("john@example.com")
     find_within_element(form, :name, "registration[phone]") |> fill_field("1111")
+    find_within_element(form, :name, "registration[residence_area]") |> fill_field("Area 1")
     find_within_element(form, :name, "registration[password]") |> fill_field("password")
     find_within_element(form, :tag, "button") |> click
 
@@ -20,11 +22,30 @@ defmodule VendyWeb.Acceptance.RegistrationTest do
 
   test "register an account with invalid/empty data" do
     navigate_to("/register")
-    form = find_element(:id, "form-registration")
+    form = find_element(:id, "registration-form")
     find_within_element(form, :tag, "button") |> click
 
     assert current_path == "/register"
     message = find_element(:class, "alert") |> visible_text()
     assert message == "Oops, something went wrong! Please check the errors below."
+  end
+
+  test "create_customer/1 returns a customer for valid data" do
+    valid_attrs = %{
+      "name" => "John Doe",
+      "email" => "johndoe@example.com",
+      "password" => "secret",
+      "residence_area" => "Area 1",
+      "phone" => "1111"
+    }
+
+    assert {:ok, customer} = CRM.create_customer(valid_attrs)
+    assert Comeonin.Bcrypt.checkpw(valid_attrs["password"], customer.password_hash)
+  end
+
+  test "create_customer/1 returns a changeset for invalid data" do
+    invalid_attrs = %{}
+
+    assert {:error, %Ecto.Changeset{}} = CRM.create_customer(invalid_attrs)
   end
 end
