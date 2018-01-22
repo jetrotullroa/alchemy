5b2a2bdea83ad078127727b33bc334ace4ce3f63
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-01-22 11:04:26 +0800

    create a photozoo app generate a Posts context with photo schema

---
+++ photozoo/lib/photozoo.ex
@@ -0,0 +1,9 @@
+defmodule Photozoo do
+  @moduledoc """
+  Photozoo keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+end
