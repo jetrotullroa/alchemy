a8ee9ce240f368b7a3b5589f8e116ac716e86438
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-01-25 14:45:42 +0800

    initial commit dolistto

---
+++ dolistto/lib/dolistto.ex
@@ -0,0 +1,18 @@
+defmodule Dolistto do
+  @moduledoc """
+  Documentation for Dolistto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dolistto.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ dolistto/test/dolistto_test.exs
@@ -0,0 +1,8 @@
+defmodule DolisttoTest do
+  use ExUnit.Case
+  doctest Dolistto
+
+  test "greets the world" do
+    assert Dolistto.hello() == :world
+  end
+end
