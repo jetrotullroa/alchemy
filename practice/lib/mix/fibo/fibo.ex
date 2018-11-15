44c6e2f45c7282b39bdabc016029a193f7ea7899
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-11-15 23:54:18 +0800

    compare function to check speed of both methods

--- practice/lib/mix/fibo/fibo.ex
+++ practice/lib/mix/fibo/fibo.ex
@@ -1,4 +1,16 @@
 defmodule Practice.Fibo do
+  def process_time(func, args, label \\ "Function") do
+    t0 = Time.utc_now()
+    f = func.(args)
+    diff = Time.diff(Time.utc_now(), t0, :millisecond)
+    IO.puts("#{label} : #{f} completed in #{diff}ms")
+  end
+
+  def compare(n \\ 45) do
+    process_time(&naive/1, n, "Naive")
+    process_time(&faster/1, n, "Faster")
+  end
+
   def naive(1), do: 1
 
   def naive(2), do: 1
