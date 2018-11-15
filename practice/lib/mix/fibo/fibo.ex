293452a0a4c9378d475b17dab359bc369424c67c
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-11-15 23:45:22 +0800

    tail rec fibonachi function

--- practice/lib/mix/fibo/fibo.ex
+++ practice/lib/mix/fibo/fibo.ex
@@ -6,4 +6,11 @@ defmodule Practice.Fibo do
   def naive(n) do
     naive(n - 2) + naive(n - 1)
   end
+
+  def faster(n), do: faster(n, 0, 1)
+  def faster(1, _acc1, acc2), do: acc2
+
+  def faster(n, acc1, acc2) do
+    faster(n - 1, acc2, acc1 + acc2)
+  end
 end
